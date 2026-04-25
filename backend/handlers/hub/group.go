81d4e6c18b9f20552d1078eb3a8d2dfb3470a23d
mingli202 <vincentmingli@gmail.com>
2026-04-24 22:14:44 -0400

    put group into its own file

---
+++ backend/handlers/hub/group.go
@@ -0,0 +1,122 @@
+package hub
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"maps"
+	"strings"
+	"sync"
+	"time"
+	"tui/backend/models"
+
+	"github.com/gorilla/websocket"
+)
+
+type Group struct {
+	mu    sync.RWMutex
+	id    string
+	users map[string]*User
+	data  models.Data
+}
+
+// Makes a new group with the given id and data
+func newGroup(id string, data models.Data) Group {
+	return Group{
+		id:    id,
+		users: make(map[string]*User),
+		data:  data,
+	}
+}
+
+// Adds the given user to this group
+func (group *Group) addUser(user *User) {
+	group.mu.Lock()
+	defer group.mu.Unlock()
+
+	group.users[user.id] = user
+	user.group = group
+}
+
+// Removes the given user to this group
+// Returns whether this group is empty
+func (group *Group) removeUser(user *User) bool {
+	group.mu.Lock()
+	defer group.mu.Unlock()
+
+	delete(group.users, user.id)
+	user.group = nil
+
+	return len(group.users) == 0
+}
+
+// avgWpm gets the average wpm of this group
+// Used to match users in relatively equal brackets
+func (group *Group) avgWpm() float64 {
+	totalWpm := 0.0
+	n := 0
+
+	for user := range maps.Values(group.users) {
+		if user != nil {
+			totalWpm += user.avgWpm()
+			n += 1
+		}
+	}
+
+	if n == 0 {
+		return 0.0
+	}
+
+	return totalWpm / float64(n)
+}
+
+// Sends a message to every user of this group
+func (group *Group) broadcast(msg string) {
+	for user := range maps.Values(group.users) {
+		if user.conn != nil {
+			user.conn.WriteMessage(websocket.TextMessage, []byte(msg))
+		}
+	}
+}
+
+// Starts the game and broadcasts updates every 1 second
+func (group *Group) startGame() {
+	minWpm := 30
+	nWords := len(strings.Split(group.data.Text, " "))
+
+	progress := make(map[string]models.Progress)
+
+	for userId := range maps.Keys(group.users) {
+		progress[userId] = models.Progress{
+			Wpm:      0,
+			Progress: 0,
+		}
+	}
+
+	ticker := time.Tick(time.Second * 1)
+	timer := time.NewTimer(time.Second * 60 * time.Duration(minWpm) * time.Duration(nWords))
+
+	countdown := 10
+
+	for {
+		select {
+		case <-ticker:
+			if countdown == 0 {
+				progressBytes, err := json.Marshal(maps.Keys(progress))
+
+				if err != nil {
+					log.Println(err)
+					break
+				}
+
+				group.broadcast("ProgressUpdate " + string(progressBytes))
+			} else {
+				group.broadcast(fmt.Sprintf("Countdown %v", countdown))
+				countdown -= 1
+			}
+		case <-timer.C:
+			break
+		}
+
+	}
+}
