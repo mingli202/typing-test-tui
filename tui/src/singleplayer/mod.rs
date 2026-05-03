fb09828213042f4565f9d319692518f48659b1ad
mingli202 <vincentmingli@gmail.com>
2026-05-03 16:33:36 -0400

    refac: singleplayer model

--- tui/src/singleplayer/mod.rs
+++ tui/src/singleplayer/mod.rs
@@ -1,10 +1,14 @@
+use ratatui::buffer::Buffer;
+use ratatui::layout::{Constraint, Rect};
 use serde::{Deserialize, Serialize};
 
-use crate::util::data_provider::Data;
+use crate::msg::Msg;
+use crate::util::data_provider::{Data, DataProvider};
 
 use self::endscreen::EndScreenModel;
 use self::typing::TypingModel;
 
+mod action;
 pub mod endscreen;
 pub mod typing;
 
@@ -42,7 +46,7 @@ impl SinglePlayerModel {
         let text = &data.text;
 
         SinglePlayerModel {
-            screen: SinglePlayerScreen::Typing(TypingModel::new(text, initial_mode)),
+            screen: SinglePlayerScreen::Typing(TypingModel::new(text, initial_mode.clone())),
             shared_model: SharedModel {
                 mode: initial_mode,
                 history: vec![],
@@ -50,4 +54,67 @@ impl SinglePlayerModel {
             },
         }
     }
+}
+
+pub fn update(
+    model: &mut SinglePlayerModel,
+    data_provider: &DataProvider,
+    msg: Msg,
+) -> Option<crate::action::Action> {
+    let mut maybe_action = match &mut model.screen {
+        SinglePlayerScreen::Typing(typing_model) => typing::Msg::from(msg).and_then(|msg| {
+            typing::update(typing_model, &mut model.shared_model, data_provider, msg)
+        }),
+        SinglePlayerScreen::End(_) => endscreen::Msg::from(msg)
+            .and_then(|msg| endscreen::update(&mut model.shared_model, data_provider, msg)),
+    };
+
+    while let Some(action) = maybe_action {
+        if let action::Action::Root(root_action) = action {
+            return Some(root_action);
+        }
+
+        maybe_action = handle_action(model, data_provider, action)
+    }
+
+    None
+}
+
+pub fn view(model: &SinglePlayerModel, area: Rect, buf: &mut Buffer) {
+    let centered = area.centered_horizontally(Constraint::Max(80));
+
+    match &model.screen {
+        SinglePlayerScreen::Typing(typing_model) => {
+            typing::view(typing_model, &model.shared_model, centered, buf)
+        }
+        SinglePlayerScreen::End(endscreen_model) => {
+            endscreen::view(endscreen_model, &model.shared_model, centered, buf)
+        }
+    };
+}
+
+pub fn handle_action(
+    model: &mut SinglePlayerModel,
+    data_provider: &DataProvider,
+    action: action::Action,
+) -> Option<action::Action> {
+    match action {
+        action::Action::ModeChange(mode) => {
+            model.shared_model.mode = mode.clone();
+
+            model.shared_model.history.clear();
+            model.shared_model.data = data_provider.get_data_from_mode(&model.shared_model.mode);
+            let text = &model.shared_model.data.text;
+
+            model.screen = SinglePlayerScreen::Typing(TypingModel::new(text, mode.clone()));
+
+            return Some(action::Action::Root(
+                crate::action::Action::ConfigModeUpdate(mode),
+            ));
+        }
+        action::Action::SwitchScreen(screen) => model.screen = screen,
+        action::Action::Root(_) => return Some(action),
+    }
+
+    None
 }
